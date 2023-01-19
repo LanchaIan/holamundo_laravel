50661833c96f8853e0f36ee6bdf4b45387775799
Ian Santana <thief.jones@gmail.com>
2023-01-19 18:10:39 +0000

    avance?

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,35 +7,39 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index() {
+        $viewData = [];
         $products = [];
+        $viewData['titulo'] = "Tienda online Ian";
+        $viewData['subtitulo'] = "Subtitulos";
+        $viewData['products'] = "products";
         $producto1= [];
         $producto1['id'] = 1;
         $producto1['name'] = "Uno";
         $producto1['description'] = "Productos";
         $producto1['image'] = "{{ asset('img/game.png') }}";
         $producto1['price'] = "Productos";
-        $products["1"] = $producto1;
+        $products[] = $producto1;
         $producto2= [];
         $producto2['id'] = 2;
         $producto2['name'] = "Productos";
         $producto2['description'] = "Productos";
         $producto2['image'] = "Productos";
         $producto2['price'] = "Productos";
-        $producto2["2"] = $producto1;
+        $producto2[] = $producto2;
         $producto3= [];
         $producto3['id'] = 3;
         $producto3['name'] = "Productos";
         $producto3['description'] = "Productos";
         $producto3['image'] = "Productos";
         $producto3['price'] = "Productos";
-        $producto3["3"] = $producto1;
+        $producto3[] = $producto3;
         $producto4= [];
         $producto4['id'] = 4;
         $producto4['name'] = "Productos";
         $producto4['description'] = "Productos";
         $producto4['image'] = "Productos";
         $producto4['price'] = "Productos";
-        $producto4["4"] = $producto1;
+        $producto4[] = $producto4;
         return view('product.index')->with("viewData", $products);
     }
 
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,13 +2,13 @@
 
 <div class="row">
     <!-- Bucle con una iteración por cada producto -->
-    @foreach ($products as $producto)
+    @foreach ($viewData["products"] as $producto)
       <div class="col-md-4 col-lg-3 mb-2">
         <div class="card">
           <img src={{$image}} class="card-img-top img-card">
           <div class="card-body text-center">
             <a href="*** ENLACE A LA RUTA QUE MOSTRARÁ EL DETALLE DEL PRODUCTO ***"
-              class="btn bg-primary text-white">{{$producto}}</a>
+              class="btn bg-primary text-white">{{$producto["name"]}}</a>
           </div>
         </div>
       </div>
